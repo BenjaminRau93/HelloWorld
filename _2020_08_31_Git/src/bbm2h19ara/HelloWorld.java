4dcaa01b58e43787de2ecb800078a3f2d3da841f
nbandrowski <nadia.bandrowski@bib.de>
2020-09-07 08:52:24 +0200

    HelloWorld message changed

--- _2020_08_31_Git/src/bbm2h19ara/HelloWorld.java
+++ _2020_08_31_Git/src/bbm2h19ara/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	
 	public static void main(String[] args) {
 		HelloWorld hw = new HelloWorld();
-		System.out.println("Hello World!");
+		System.out.println("Hello World! Hello World!!!");
 	}
 
 }
