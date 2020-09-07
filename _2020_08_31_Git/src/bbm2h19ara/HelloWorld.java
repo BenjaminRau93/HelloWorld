d58e29cc0a2200725ea62b832f5da7be07e8b426
BenjaminRau93 <ben@theraus.org>
2020-09-07 09:14:49 +0200

    added "test1" message

--- _2020_08_31_Git/src/bbm2h19ara/HelloWorld.java
+++ _2020_08_31_Git/src/bbm2h19ara/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		HelloWorld hw = new HelloWorld();
 		System.out.println("Hello World! Hello World!!!1234");
+		System.out.println("test1");
 	}
 
 }
